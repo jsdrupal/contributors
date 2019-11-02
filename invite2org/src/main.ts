5de275276979feb446d4445927b13ea1d7174203
Daniel Wehner <dawehner@googlemail.com>
2019-11-02 16:35:57 +0100

    teest

--- invite2org/src/main.ts
+++ invite2org/src/main.ts
@@ -8,6 +8,8 @@ async function run() {
     const username = process.env.GITHUB_ACTOR || '';
     const team_slug = process.env.TEAM_SLUG || '';
 
+    console.log(`Add ${username} to  ${repository} into ${team_slug}`);
+
     const octokit = new github.GitHub(token);
     const user = await octokit.users.getByUsername({
       username
@@ -16,6 +18,7 @@ async function run() {
     const match = repository.match('([^/]*)\/([^/]*)');
     if (match) {
       const org = match[0];
+      console.log({org});
       let team_ids : Array<number> = [];
       if (team_slug) {
         const team = await octokit.teams.getByName({
@@ -25,14 +28,18 @@ async function run() {
         team_ids.push(team.data.id);
       }
 
+      console.log(user.data.id);
+      console.log({team_ids});
       await octokit.orgs.createInvitation({
         org,
         invitee_id: user.data.id,
         team_ids,
       });
+      process.exit(0);
     }
   } catch (error) {
-    core.setFailed(error.message);
+    console.error(error);
+    process.exit(1);
   }
 }
 
